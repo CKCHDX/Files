1f86e81873a1864609022298b18de948839bb977
Alex Jonsson <153451620+CKCHDX@users.noreply.github.com>
2025-08-24 20:41:14 +0200

    Update efilib.h

--- HACOS/OS/efilib.h
+++ HACOS/OS/efilib.h
@@ -1,15 +1,2 @@
-#ifndef EFILIB_H
-#define EFILIB_H
-#include "efi.h"
-
-#define EFI_SUCCESS 0
-
-// Simplified EFI entry point macro
-#define EFIAPI __attribute__((ms_abi))
-
-// Minimal console output protocol
-typedef struct {
-    EFI_STATUS (*OutputString)(void *This, CHAR16 *String);
-} SIMPLE_TEXT_OUTPUT_INTERFACE;
-
-#endif
+#pragma once
+#include <efilib.h>
