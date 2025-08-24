8e3a963b6824bb7160dd09e969f881445d075cd2
Alex Jonsson <153451620+CKCHDX@users.noreply.github.com>
2025-08-24 20:41:03 +0200

    Update efi.h

--- HACOS/OS/efi.h
+++ HACOS/OS/efi.h
@@ -1,14 +1,2 @@
-#ifndef EFI_H
-#define EFI_H
-
-#include <stdint.h>
-
-typedef uint64_t EFI_STATUS;
-typedef void *EFI_HANDLE;
-typedef uint16_t CHAR16;
-
-typedef struct {
-    void *ConOut; // Simple console output (for early testing)
-} EFI_SYSTEM_TABLE;
-
-#endif
+#pragma once
+#include <efi.h>
